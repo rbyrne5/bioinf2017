6d14d3f0ce54e7c824097d621627f5491c1017b3
rbyrne5 <rbyrne5@tcd.ie>
2017-03-02 16:10:13 +0000

    Update and rename convergence_tester-copy.R to convergence_test_plotter.R

--- convergence_test_plotter.R
+++ convergence_test_plotter.R
@@ -1,4 +1,7 @@
-
+###########################
+#Note this is not a self sufficient script, run each line of code on objects created by Lawson's script
+#First generate two meancoincidence files using Lawson's plotting script.
+#Also need a tdend object from it
 ##Plotting both matrixes on one diagonal ordered by a single tree
 ##requires that you have built tdend as in lawson script and loaded two mean coincidence matrixes
 fullorder<-labels(tdend) # the order according to the tree, choose one.
